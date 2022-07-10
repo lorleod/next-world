6f859f67fbe3d3979b5972774cd0054af2e14e28
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-07-10 12:02:43 -0700

    connecting to database and allows insert

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongo = require("./mongo");
+
+const UsersSchema = require("./models/Users");
+
+app.use(express.json());
+
+const connectToMongo = async () => {
+  await mongo().then(async (mongoose) => {
+    try {
+      console.log("connecting to mongodb");
+      const user = {
+        Username: "Kevin",
+        Password: "password",
+      };
+
+      await new UsersSchema(user).save();
+    } finally {
+      mongoose.connection.close();
+    }
+  });
+};
+
+connectToMongo();
+
+app.listen(port, () => {
+  console.log(`server is runn on ${port}...`);
+});
