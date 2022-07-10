533f2af807b4462f658555df744f92018e95f648
Taylor McLeod <Taylor@xyloMac.hitronhub.home>
2022-07-09 17:17:22 -0700

    added username and password and setup .env

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const vrchat = require("vrchat");
+
+import { Routes, Route } from "react-router-dom";
+import { USERNAME, PASSWORD } from './.env'
+
 import "./App.scss";
 import Nav from "./components/partials/Nav";
 import Home from "./components/Home";
@@ -5,7 +10,13 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import AddWorld from "./components/AddWorld";
 import User from "./components/User";
-import { Routes, Route } from "react-router-dom";
+
+
+// Step 1. We begin with creating a Configuration, which contains the username and password for authentication.
+const configuration = new vrchat.Configuration({
+    username: USERNAME,
+    password: PASSWORD
+});
 
 function App() {
   return (
