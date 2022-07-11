543cc2d589d9636f9c6e3e11450a56b32a495350
Taylor McLeod <Taylor@xyloMac.local>
2022-07-11 10:27:01 -0700

    vrchat api sdk authentication working

--- server/index.js
+++ server/index.js
@@ -2,8 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const mongo = require("./mongo");
-
+require('dotenv').config()
 const UsersSchema = require("./Schema/users-schema");
+
+// Step 1. We begin with creating a Configuration, which contains the username and password for authentication.
+const vrchat = require("vrchat");
+const configuration = new vrchat.Configuration({
+    username: process.env.VRCUSER,
+    password: process.env.VRCPASSWORD
+});
+
+// Step 2. VRChat consists of several API's (WorldsApi, UsersApi, FilesApi, NotificationsApi, FriendsApi, etc...)
+// Here we instantiate the Authentication API which is required for logging in.
+const AuthenticationApi = new vrchat.AuthenticationApi(configuration);
+
+// Step 3. Calling getCurrentUser on Authentication API logs you in if the user isn't already logged in.
+AuthenticationApi.getCurrentUser()
+  .then(resp => {
+    const currentUser = resp.data;
+    console.log(`Logged in as: ${currentUser.displayName}`)
+}).catch(error => {
+  console.log("ERROR:", error)
+});
+
+
 
 app.use(express.json());
 
