07109231d57d41b3d73bd814c968f38a6374ce84
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-07-11 10:31:54 -0700

    adding some comments

--- server/index.js
+++ server/index.js
@@ -6,12 +6,15 @@ const mongoose = require("mongoose");
 const usersRoute = require("./routes/users");
 const loginRoute = require("./routes/login");
 
+//Middleware
 app.use(express.json());
 app.use(cors());
 
+//Routes
 app.use("/user/register", usersRoute);
 app.use("/user/login", loginRoute);
 
+//Connecting to MongoDB
 mongoose.connect(
   "mongodb+srv://NextWorld:KRT176LHL@nextworldcluster.i7wjs.mongodb.net/NextWorldDB?retryWrites=true&w=majority",
   {
@@ -19,6 +22,7 @@ mongoose.connect(
   }
 );
 
+//Main server
 app.listen(port, () => {
   console.log(`server is running on ${port}...`);
 });
