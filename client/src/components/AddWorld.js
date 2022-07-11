8ea7554018330d3ab6ea48199653697b5c7e67d3
Taylor McLeod <Taylor@xyloMac.local>
2022-07-11 16:01:34 -0700

    last commit + a file

--- client/src/components/AddWorld.js
+++ client/src/components/AddWorld.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import SearchBar from "./partials/SearchBar";
 import Api from "../api/Api";
 import WorldList from "./partials/WorldList";
+import axios from "axios";
 
 function AddWorld() {
   //Creates state and sets to empty array
@@ -13,13 +14,20 @@ function AddWorld() {
 
   //Takes input field and adds submit to API url
   const onSearch = async (text) => {
-    const results = await Api.get("/", {
-      params: { q: text },
-    });
+    // const results = await Api.get("/", {
+    //   params: { q: text },
 
-    setState((prevState) => {
-      return { ...prevState, results: results };
-    });
+    console.log("in onSearch - text: ", text);
+    const response = await axios.post("http://localhost:3001/api/getWorld", {text: text})
+      .then((response) => {
+        console.log("search results: ", response);
+      });
+
+    // };
+
+    // setState((prevState) => {
+    //   return { ...prevState, results: results };
+    // });
   };
 
   return (
