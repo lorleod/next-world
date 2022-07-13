6d6e9ff40b5c88dc5726b328638e2b4b1d3d2728
Taylor McLeod <Taylor@xyloMac.local>
2022-07-12 17:07:52 -0700

    minor

--- server/routes/playlist.js
+++ server/routes/playlist.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
+const { WorldsApi } = require("../vrcApi");
+
+router.post("/:worldID", async (req, res) => {
+  let worldId = req.params.worldID;
+
+})
 
 module.exports = router;
