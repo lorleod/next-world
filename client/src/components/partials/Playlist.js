00c1071c39f9ea8a78106520571769929b2a22a5
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-07-12 18:18:30 -0700

    showing playlist information based on url

--- client/src/components/partials/Playlist.js
+++ client/src/components/partials/Playlist.js
@@ -1,26 +1,29 @@
 import World from "./World";
 import WorldExpanded from "./WorldExpanded";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 function Playlist({ results }) {
-  // console.log("results at worldlist", results.data)
-  // //Makes data empty array so not read as undefined
-  // let data = [];
-  // //Changes data to results if present
-  // if (results.data) {
-  //   data = results.data;
-  // }
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const params = useParams();
+  const playlistId = params.id;
 
-  // console.log("Playlist data", data)
+  axios.get(`http://localhost:3001/playlist/${playlistId}`).then((response) => {
+    setTitle(response.data.title);
+    setDescription(response.data.description);
+  });
 
-  // //Maps through data and adds as prop to world.js
-  // let playlist = data.map((item) => <WorldExpanded
-  //   key={item.id}
-  //   world={item}
-  //   title={item.name}
-  //   image={item.thumbnailImageUrl}
-  //   author={item.authorName}
-  // />);
-  return (<div className="result"><p>HITest</p></div>);
+  return (
+    <div className="result">
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <h3>
+        <Link to="/addworld">Add World</Link>
+      </h3>
+    </div>
+  );
 }
 
 export default Playlist;
--- server/routes/playlist.js
+++ server/routes/playlist.js
@@ -1,9 +1,37 @@
 const router = require("express").Router();
+const User = require("../Schema/playlists-schema");
 const { WorldsApi } = require("../vrcApi");
+const Playlist = require("../Schema/playlists-schema");
 
-router.post("/:worldID", async (req, res) => {
-  let worldId = req.params.worldID;
+router.get("/:playlistID", async (req, res) => {
+  let playlistId = req.params.playlistID;
 
-})
+  console.log("playlistID: ", playlistId);
+
+  let ObjectId = require("mongodb").ObjectId;
+  let o_id = new ObjectId(playlistId);
+
+  const playlist = await Playlist.findOne({ _id: o_id });
+
+  console.log("PLAYLIST: ", playlist);
+  res.send(playlist);
+
+  // request to VRC API getWorld
+  // let worldArray = [
+  //   "wrld_c7407bed-0a85-4608-8906-3c3ac2f2fb5a",
+  //   "wrld_791ebf58-54ce-4d3a-a0a0-39f10e1b20b2",
+  // ];
+  // let worldArray = playlist.worldIds;
+
+  // map array of worldIDs to an array of world objects
+  // worldArray.map((worldId) => {
+  //   WorldsApi.getWorld(worldID);
+  //   // .then((worldResult) => {
+  //   //   return worldResult
+  //   // });
+  // });
+
+  // return res.json({ status: "ok" });
+});
 
 module.exports = router;
