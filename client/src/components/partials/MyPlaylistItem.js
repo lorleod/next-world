9f94582eb68aa6832453d3a685159cd42c1f3ca0
Taylor McLeod <Taylor@xyloMac.local>
2022-07-14 21:16:41 -0700

    renamed userplaylistitem to myplaylistitem. Added comments, added error handling for backend request, removed console.logs

--- client/src/components/partials/MyPlaylistItem.js
+++ client/src/components/partials/MyPlaylistItem.js
@@ -2,27 +2,34 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+// component to display each individual playlist created by current user
 export default function MyPlaylistItem(props) {
   const playlistUrl = `/playlist/${props.PlaylistId}`;
+
+  // pop up a confirmation then delete playlist if confirmed by user
   const deletePlaylist = () => {
+    // confirm equals true if user clicks "ok" on pop up
     let confirm = window.confirm(
       "Are you sure you want to delete this playlist?"
     );
+
+    //if confirm equals true, send DELETE request to backend
     if (confirm) {
-      console.log("delete");
       const response = axios
         .delete(`http://localhost:3001/playlist/delete`, {
           data: { _id: props.PlaylistId },
         })
         .then((response) => {
           const data = response.data;
-          console.log("data in playlistitem: ", data);
+          //if response confirms delete, alert user then redirect back to dashboard
           if (data === "deleted") {
             alert("Playlist deleted");
             window.location.href = "/user";
           }
         })
-        .catch((error) => {});
+        .catch((error) => {
+          console.log("Error deleting playlist: ", error);
+        });
     }
   };
 
