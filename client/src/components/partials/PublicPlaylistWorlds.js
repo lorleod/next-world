5d67eb31be1eceb12fbcfd3e76469c766b16c527
Kevin Holmes <kevinholmes@Kevins-MacBook-Pro.local>
2022-07-15 12:55:03 -0700

    home page images showing with scrolling

--- client/src/components/partials/PublicPlaylistWorlds.js
+++ client/src/components/partials/PublicPlaylistWorlds.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import publicPLayistCard from "./PublicPlaylistCard";
 
 const PublicPlaylistWorlds = (props) => {
   // console.log("props PPW: ", props.worldId);
   const [worldTitle, setWorldTitle] = useState("");
+  const [worldImage, setWorldImage] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       await axios
         .get(`http://localhost:3001/api/getWorld/${props.worldId}`)
         .then((response) => {
-          // console.log("response.data.title ", response.data.name);
+          // console.log("response.data.title ", response.data.thumbnailImageUrl);
           setWorldTitle(response.data.name);
+          setWorldImage(response.data.thumbnailImageUrl);
         })
         .catch((error) => {});
     };
@@ -21,7 +24,10 @@ const PublicPlaylistWorlds = (props) => {
   // console.log("worldTitle: ", worldTitle);
   return (
     <div>
-      <div>{worldTitle}</div>
+      {/* <div>{worldTitle}</div> */}
+      <div className="public-playlist-image-container">
+        <img className="public-playlist-image" src={worldImage} alt="world" />
+      </div>
     </div>
   );
 };
