a1dae5b3589714be56820e446bdd3d5f5e0735c7
Robert Di Ninno <robertdininno@Roberts-MacBook-Pro.local>
2022-07-14 18:06:35 -0700

    fix extra spacing on login and register page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,7 +34,6 @@ function Login() {
   };
 
   return (
-    <div className="App">
       <div id="login-page-container">
         <h1 id="login-page-heading">Login</h1>
         <div className="login-form-container">
@@ -62,7 +61,6 @@ function Login() {
           </form>
         </div>
       </div>
-    </div>
   );
 }
 
